53ab3df2035595090d4782473ee9dec27447007a
dliuw <davidjliuw@gmail.com>
2020-09-26 22:53:27 +0800

    Style : change tab size

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,14 +1,16 @@
 class DataSource {
-	static getCategory(){
-		return fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-		.then(response =>{
-			return response.json();
-		})
-		.then(responseJson => {
-			return Promise.resolve(responseJson);
-		})
-		catch(error => {
-			return Promise.reject('Error');
-		})
-	}
+  static getCategory() {
+    return fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        return Promise.resolve(responseJson);
+      })
+      .catch((error) => {
+        return Promise.reject("Error");
+      });
+  }
 }
+
+export default DataSource;
