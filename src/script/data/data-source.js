d31ade9e7ddc4ed96cfde050d2d3f074c38ace38
dliuw <davidjliuw@gmail.com>
2020-09-27 23:06:19 +0800

    Feature : get category & search item

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,16 +1,23 @@
 class DataSource {
   static getCategory() {
-    return fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-      .then((response) => {
-        return response.json();
-      })
+    return fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+      .then((response) => response.json())
+      .then((responseJson) => Promise.resolve(responseJson))
+      .catch((error) => Promise.reject('Error'));
+  }
+
+  static searchItem(keyword) {
+    return fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`
+    )
+      .then((response) => response.json())
       .then((responseJson) => {
-        return Promise.resolve(responseJson);
-      })
-      .catch((error) => {
-        return Promise.reject("Error");
+        if (responseJson.meals) {
+          return Promise.resolve(responseJson.meals);
+        } else {
+          return Promise.reject(`${keyword} not found`);
+        }
       });
   }
 }
-
 export default DataSource;
