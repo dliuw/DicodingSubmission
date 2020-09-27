7b14aca1315dbcd8cec95ada5a917265eee417ca
dliuw <davidjliuw@gmail.com>
2020-09-27 23:05:54 +0800

    Build : importing custom element and other script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
+import './script/component/top-bar.js';
+import './script/component/search-bar.js';
+import './script/component/nav-category.js';
+import main from './script/view/main.js';
+
+document.addEventListener('DOMContentLoaded', main);
